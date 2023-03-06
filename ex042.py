40013d9780056f19c1cb1e88fb92289f5054dbba
Andrey <andrey.delbello2002@gmail.com>
2023-03-06 14:30:28 -0300

    exercicios

--- ex042.py
+++ ex042.py
@@ -9,6 +9,12 @@ c3 = float(input('Digite o terceiro segmento: '))
 if (c1 + c2) > c3 and (c1 + c3) > c2 and (c2 + c3) > c1:
     if c1 == c2 == c3:
         print('Os segmentos acima PODEM formar um triângulo EQUILATERO!')
+    elif c1 == c2 != c3 or c1 == c3 != c2 or c1 != c2 == c3:
+        print('Os segmentos acima PODEM formar um triângulo ISÓSCELES!')
+    elif c1 != c2 != c3:
+        print('Os segmentos acima PODEM formar um triângulo ESCALENO!')
+else:
+    print('Os segmentos acima NÃO formam um TRIÂNGULO!')
     
 
         
